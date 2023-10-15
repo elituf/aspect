648be10e229f6afe06688d615d57c2729faeea8d
futile <150585291+elituf@users.noreply.github.com>
2023-10-15 22:57:38 +0100

    0.5.0
    - changed stuff

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
-#[allow(unused)]
-use colored::Colorize;
 use num::integer::gcd;
 
 mod args;
 
-// you may ask, "oh why do we need so many functions?!@1!@!>?>/"
-// shut the helll UP
 fn calc_aspect(w: usize, h: usize) -> String {
     format!("{}:{}", w / gcd(w, h), h / gcd(w, h),)
-}
-
-fn calc_aspect_res(w: usize, h: usize) {
-    println!(
-        "Resolution: {}\nAspect ratio: {}\n",
-        format!("{w}x{h}"),
-        calc_aspect(w, h),
-    );
 }
 
 fn calc_aspect_image(path: String) {
@@ -24,9 +12,7 @@ fn calc_aspect_image(path: String) {
             let w = size.width;
             let h = size.height;
             println!(
-                "File: {}\nResolution: {}\nAspect ratio: {}\n",
-                format!("{path}"),
-                format!("{w}x{h}"),
+                "File: {path}\nResolution: {w}x{h}\nAspect ratio: {}\n",
                 calc_aspect(w, h),
             );
         }
@@ -38,9 +24,10 @@ fn main() {
     let args: args::Args = argh::from_env();
 
     if args.width.is_some() && args.height.is_some() {
-        calc_aspect_res(args.width.unwrap(), args.height.unwrap());
+        let w = args.width.unwrap();
+        let h = args.height.unwrap();
+        println!("Resolution: {w}x{h}\nAspect ratio: {}\n", calc_aspect(w, h));
     }
-
     if args.image.is_some() {
         calc_aspect_image(args.image.unwrap());
     }
