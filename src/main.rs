98910dc08bd9f5e2af1d09bb827a1c81d7a68f71
futile <150585291+elituf@users.noreply.github.com>
2023-10-28 04:32:55 +0100

    0.7.0
    - change crate `num` to `gcd` so that we don't compile a bunch of useless shit to calculate gcd 4 times
    - make the printed path for image ACTUALLY absolute (it is horribly implemented)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use num::integer::gcd;
+use gcd::Gcd;
 use std::path::PathBuf;
 
 mod args;
 
 fn calc_aspect(w: usize, h: usize) -> String {
-    format!("{}:{}", w / gcd(w, h), h / gcd(w, h),)
+    format!("{}:{}", w / w.gcd(h), h / w.gcd(h),)
 }
 
 fn calc_aspect_image(path: PathBuf) {
@@ -13,7 +13,12 @@ fn calc_aspect_image(path: PathBuf) {
             let w = size.width;
             let h = size.height;
             let working_path = std::env::current_dir().expect("could not get working path");
-            println!("File: {}\nResolution: {w}x{h}\nAspect ratio: {}\n", working_path.join(path).display(), calc_aspect(w, h),);
+            println!(
+                "File: {}\nResolution: {w}x{h}\nAspect ratio: {}\n",
+                // this is so fucked up but i'm too lazy right now to find a better way
+                working_path.join(path.canonicalize().unwrap()).to_string_lossy().trim_start_matches(r"\\?\"),
+                calc_aspect(w, h),
+            );
         }
         Err(why) => println!("{why}"),
     }
