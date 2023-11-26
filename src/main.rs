8a81d0473b813d45930a7db63bea6ed3d93dcea5
futile <150585291+elituf@users.noreply.github.com>
2023-11-26 15:24:42 +0000

    nicely destructure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-mod args;
-mod gcd;
-
+use args::Args;
 use clap::Parser;
 use eyre::Result;
 use gcd::gcd;
+use imagesize::ImageSize;
 use std::path::PathBuf;
+
+mod args;
+mod gcd;
 
 fn calc_aspect(w: usize, h: usize) -> String {
     format!("{}:{}", w / gcd(w, h), h / gcd(w, h),)
@@ -12,25 +14,23 @@ fn calc_aspect(w: usize, h: usize) -> String {
 
 fn calc_aspect_image(path: &PathBuf) -> Result<String> {
     match imagesize::size(path) {
-        Ok(size) => Ok(format!(
-            "File: {}\nResolution: {}x{}\nAspect ratio: {}",
-            path.canonicalize()?.to_string_lossy().trim_start_matches(r"\\?\"),
-            size.width,
-            size.height,
-            calc_aspect(size.width, size.height),
+        Ok(ImageSize { width, height }) => Ok(format!(
+            "File: {}\nResolution: {width}x{height}\nAspect ratio: {}",
+            path.display(),
+            calc_aspect(width, height),
         )),
         Err(why) => Err(why.into()),
     }
 }
 
 fn main() {
-    let args = args::Args::parse();
+    let args = Args::parse();
 
     if let (Some(w), Some(h)) = (args.width, args.height) {
         println!("Resolution: {w}x{h}\nAspect ratio: {}", calc_aspect(w, h));
     }
     if args.width.is_some() && args.height.is_some() && args.image.is_some() {
-        println!();
+        println!(); // print an empty line should the user use all the arguments
     }
     if let Some(img) = args.image {
         match calc_aspect_image(&img) {
